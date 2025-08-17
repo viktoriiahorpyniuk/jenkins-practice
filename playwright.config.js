9928517113fd9f1406a58f8e2e4d3019af5a6dfa
Vicky_H <161350197+viktoriiahorpyniuk@users.noreply.github.com>
2025-08-17 11:29:32 -0400

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: 1,
   workers: 4,
